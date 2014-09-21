24df945a9a0f2412eb9d460e4cb4c83a1c145151
Will Burgo <wburgo999@gmail.com>
2014-09-20 18:32:25 -0700

    practice quiz for self-assessment on Monday

---
+++ review_materials/practice_quiz_week2/extra_credit/extra.js
@@ -0,0 +1,4 @@
+/*
+Implement an algorithm that solves nQueens
+for n = 27 in under 750 milliseconds.
+*/
---
+++ review_materials/practice_quiz_week2/underbar/filter.js
@@ -0,0 +1,24 @@
+/* Filter takes an array and returns an array of all elements that pass a truth test.
+		
+		Example: 
+		filter([1,2,3,4], function(item) {return item % 2 === 0});
+		>> [2,4]
+
+*/
+  var filter = function(array, test) {
+  };
+
+
+  /*
+		Now write a function, Uniq, which takes an array, and returns a copy of this array 
+		without duplicates.
+
+		Example: uniq([1,1,2,2,3,3,4])
+		>> [1,2,3,4]
+
+		USE FILTER IN YOUR CODE
+  */
+
+  var uniq = function(array) {
+
+  };
